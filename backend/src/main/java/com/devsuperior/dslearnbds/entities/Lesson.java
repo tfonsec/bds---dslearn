7c4658f75b42c39b31b8e0de31d0c727aa78cc50
Thiago Fonseca <thifonseca85@gmail.com>
2021-11-17 18:17:04 -0300

    Deliver

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Lesson.java
@@ -1,11 +1,12 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import javax.persistence.Entity;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -15,6 +16,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -38,6 +40,9 @@ public abstract class Lesson implements Serializable {
 			@JoinColumn(name = "user_id"), @JoinColumn(name = "offer_id") })
 	private Set<Enrollment> enrollmentsDone = new HashSet<>();
 
+	@OneToMany(mappedBy = "lesson")
+	private List<Deliver> deliveries = new ArrayList<>();	
+	
 	public Lesson() {
 
 	}
--- backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/User.java
@@ -34,7 +34,7 @@ public class User implements Serializable {
 	private Set<Role> roles = new HashSet<>();
 
 	@OneToMany(mappedBy = "user")
-	private List<Notification> notification = new ArrayList<>();
+	private List<Notification> notifications = new ArrayList<>();
 	
 	public User() {
 
