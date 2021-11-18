adaaeba6f2939d592bba08ba9d1f33657975474b
Thiago Fonseca <thifonseca85@gmail.com>
2021-11-18 20:34:10 -0300

    Repository

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.devsuperior.dslearnbds.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.devsuperior.dslearnbds.entities.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	
+}
