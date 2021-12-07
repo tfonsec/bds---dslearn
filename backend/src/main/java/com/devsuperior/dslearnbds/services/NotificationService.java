7bc5dccf8232cba05c27f530c194ab7a8ca00c6b
Thiago Fonseca <thifonseca85@gmail.com>
2021-12-06 22:52:05 -0300

    Observer design pattern, OCP SOLID principle

--- backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -1,30 +1,70 @@
 package com.devsuperior.dslearnbds.services;
 
+import java.time.Instant;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.entities.Deliver;
 import com.devsuperior.dslearnbds.entities.Notification;
 import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.observers.DeliverRevisionObserver;
 import com.devsuperior.dslearnbds.repositories.NotificationRepository;
 
-
-
 @Service
-public class NotificationService {
+public class NotificationService  implements DeliverRevisionObserver {
 
-    @Autowired
+	@Autowired
 	private NotificationRepository repository;
-    
-    @Autowired
-    private AuthService authService;
-    
-    public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pegealbe) {
-    	User user =  authService.authenticated();
-    	Page<Notification> page =  repository.find(user, unreadOnly, pegealbe);
-    	return page.map(x -> new NotificationDTO(x));
-    	
-    }
+
+	@Autowired
+	private AuthService authService;
+	
+	@Autowired
+	private DeliverService deliverservice;
+	
+	
+	@PostConstruct
+	private void initialize() {
+		deliverservice.subscribeDeliverRevisionObserver(this);
+		
+	}
+
+	@Transactional(readOnly = true)
+	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pegealbe) {
+		User user = authService.authenticated();
+		Page<Notification> page = repository.find(user, unreadOnly, pegealbe);
+		return page.map(x -> new NotificationDTO(x));
+
+	}
+
+
+	@Transactional
+	public void saveDeliverNotification(Deliver deliver) {
+
+		Long sectionId = deliver.getLesson().getSection().getId();
+		Long resourceId = deliver.getLesson().getSection().getResource().getId();
+		Long offerId = deliver.getLesson().getSection().getResource().getOffer().getId();
+
+		String route = "/offers/" + offerId + "/resources/" + resourceId + "/sections/" + sectionId;
+		String text = deliver.getFeedback();
+		Instant moment = Instant.now();
+		User user = deliver.getEnrollment().getStudent();
+
+		Notification notification = new Notification(null, text, moment, false, route, user);
+		repository.save(notification);
+
+	}
+
+	@Override
+	public void onSaveRevision(Deliver deliver) {
+		saveDeliverNotification(deliver);
+		
+	}
 }
